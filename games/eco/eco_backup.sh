f2d651df590bb9abeab0ef8e119fb29ac4d84375
Roman Yakushev <rykushev@gmail.com>
2023-04-30 23:22:01 +0700

    eco inprogress

--- games/eco/eco_backup.sh
+++ games/eco/eco_backup.sh
@@ -2,20 +2,20 @@
 whoami
 systemctl stop eco.service
 sleep 10
-today=`date '+%Y_%m_%d-%H-%M'`;
+today=$(date '+%Y_%m_%d-%H-%M');
 folder="$BACKUP_FOLDER/eco";
 tmpfolder='/var/backups/eco/';
 day=$( date +%d );
 filename="4fun4_eco_${today}.tar.gz";
-tar -zcf ${tmpfolder}/daily/${filename} /home/eco/server/Storage
+tar -zcf "${tmpfolder}/daily/${filename}" /home/eco/server/Storage
 systemctl start eco.service
 
-cp ${tmpfolder}/daily/${filename} ${folder}/daily/${filename}
-rm -f ${tmpfolder}/daily/${filename}
+cp "${tmpfolder}/daily/${filename}" "${folder}/daily/${filename}"
+rm -f "${tmpfolder}/daily/${filename}"
 
-find ${tmpfolder}/daily/ -mtime +7 -type f -name "*.gz" -delete
-find ${folder}/daily/ -mtime +7 -type f -name "*.gz" -delete
+find "${tmpfolder}/daily/" -mtime +7 -type f -name "*.gz" -delete
+find "${folder}/daily/" -mtime +7 -type f -name "*.gz" -delete
 if [ "$day" = "01" ]; then
-	cp ${folder}/daily/${filename} ${folder}/monthly/${filename}
-	find ${folder}/monthly/ -mtime +95 -type f -name "*.gz" -delete
+	cp "${folder}/daily/${filename}" "${folder}/monthly/${filename}"
+	find "${folder}/monthly/" -mtime +95 -type f -name "*.gz" -delete
 fi
