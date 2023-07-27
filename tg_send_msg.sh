4eddc2f439c12700f4d525d5a2a8dd1eda43f9c3
Roman Yakushev <rykushev@gmail.com>
2023-07-28 00:50:52 +0700

    try ssh login script

--- tg_send_msg.sh
+++ tg_send_msg.sh
@@ -4,6 +4,9 @@ today=`date '+%Y.%m.%d-%H:%M:%S'`;
 
 curl -g \
   --data-urlencode "chat_id=$TG_CHAT_ID" \
-  --data-urlencode "text=*${today}* $1\`\`\` $2 $3 $4 $5\`\`\`" \
+  --data-urlencode "text=*${today}* $1\`\`\` $2
+$3
+$4
+$5\`\`\`" \
   --data-urlencode "parse_mode=markdown" \
   "https://api.telegram.org/bot$TG_API_KEY/sendMessage"
