0c8abdd46e402fad398c4cb577541533a4922dd5
Roman Yakushev <rykushev@gmail.com>
2024-04-12 14:43:09 +0700

    rust update 7

--- games/rust/start.sh
+++ games/rust/start.sh
@@ -15,10 +15,10 @@ exec ./RustDedicated -batchmode \
   +server.description "RU\EN 4fun Для новичков for newbies solo duo trio
 
 - Rust+
-- 4250 размер карты
+- 4000 размер карты
 - Классика, без модов, без донатов и прочей херни. Ничего нельзя купить.
 - Админ если что то выдаст, вы увидите сообщение в чате (но он этого не будет делать)
-- Вайп первый четверг месяца (global)
+- Вайп дважды в месяц, следующий - 18.04. Прошлый - 11.04 был по техническим причинам (лагала карта)
 - Мощный античит
 - Максимум 3 человека в команде!
 - Желаем приятной, честной игры!
@@ -26,11 +26,11 @@ exec ./RustDedicated -batchmode \
 - 4250 card size
 - Classic, no mods, no donations and other crap. You can't buy anything.
 - If the admin gives something, you will see a message in the chat (but he won't do that)
-- Wipe first Thursday of the month (global)
+- Wipe twice a month, the next one is 04/18. The previous one - 11.04 was due to technical reasons (the map was laggy)
 - Powerful anti-cheat
 - Maximum 3 people per team!
 - We wish you a pleasant, fair game!
 - Cupboard resource consumption reduced by 50%" \
   +server.url "https://discord.gg/7QnBbwJG4e" \
   +server.identity "4fun42" \
-  +server.tags "EU,monthly,vanilla,tut,WA,EA"
+  +server.tags "EU,biweekly,vanilla,tut,WA,EA"
