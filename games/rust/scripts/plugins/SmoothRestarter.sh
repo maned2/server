805a916a86b9a24dbf62aac78ac3540d042637a9
Roman Yakushev <rykushev@gmail.com>
2024-04-20 00:56:18 +0700

    rust update 8

--- games/rust/scripts/plugins/SmoothRestarter.sh
+++ games/rust/scripts/plugins/SmoothRestarter.sh
@@ -9,6 +9,6 @@ if [ ! -f "$FILE" ]; then
     cp server/oxide/config/SmoothRestarter.json FILE
 fi
 
-sed -i -- 's/"0:00"/"1:00"/g' server/oxide/config/SmoothRestarter.json
+sed -i -- 's/"0:00"/"9:00"/g' server/oxide/config/SmoothRestarter.json
 sed -i -- 's/"Restart when new Oxide.Rust is out": true,/"Restart when new Oxide.Rust is out": false,/g' server/oxide/config/SmoothRestarter.json
 EOF
