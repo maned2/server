0c9847750775111fc93e7597a6b2aefe6820e98e
Roman Yakushev <rykushev@gmail.com>
2024-12-04 04:52:32 +0700

    rust update

--- games/rust/scripts/cfg_write.sh
+++ games/rust/scripts/cfg_write.sh
@@ -4,9 +4,16 @@ sudo -i -u rust bash << EOF
 mkdir -p server/server/4fun42/cfg/
 
 echo "ownerid $RUST_ADMIN_STEAM_ID_1 \"unnamed\" \"no reason\"
+ownerid $RUST_ADMIN_STEAM_ID_2 \"unnamed\" \"no reason\"" > server/server/4fun42/cfg/users.cfg
+
+echo "wipetimer.wipetimezone: \"Asia/Krasnoyarsk\"" > server/server/4fun42/cfg/server.cfg
+
+
+mkdir -p server/server/4fun43/cfg/
+
+echo "ownerid $RUST_ADMIN_STEAM_ID_1 \"unnamed\" \"no reason\"
 ownerid $RUST_ADMIN_STEAM_ID_2 \"unnamed\" \"no reason\"" > server/server/4fun43/cfg/users.cfg
 
 echo "wipetimer.wipetimezone: \"Asia/Krasnoyarsk\"" > server/server/4fun43/cfg/server.cfg
-
 EOF
 
