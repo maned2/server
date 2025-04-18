1eba6ae370c2b14f21a68e1e501da134f4ec9938
Roman Yakushev <rykushev@gmail.com>
2025-04-18 15:28:30 +0700

    rust update

--- games/rust/scripts/cron.sh
+++ games/rust/scripts/cron.sh
@@ -2,17 +2,17 @@
 
 cd $RUST_SCRIPTS_FOLDER
 
-sh ./stop.sh
-sh ./ports_close.sh
+#sh ./stop.sh
+#sh ./ports_close.sh
 
 sh ./backup.sh
 
-sh ./update_rust.sh
-sh ./cfg_write.sh
+#sh ./update_rust.sh
+#sh ./cfg_write.sh
 #sh ./update_oxide.sh
 #sh ./update_plugins_all.sh
 #sh ./cfg_write.sh
 
-sh ./start.sh
-sh ./ports_open.sh
+#sh ./start.sh
+#sh ./ports_open.sh
 sh ./backup_move.sh
